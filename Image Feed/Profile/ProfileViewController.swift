cef65543f81d3a4438872ef76348924b9ff367c1
smoke0030 <ft.priest@gmail.com>
2023-09-19 21:26:50 +0300

    добавлены аутлеты к элементам профайла

--- Image Feed/Profile/ProfileViewController.swift
+++ Image Feed/Profile/ProfileViewController.swift
@@ -1,12 +1,20 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var userAvatar: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
       
     }
     
-
+    @IBAction func exitButton(_ sender: Any) {
+    }
+    
 }
