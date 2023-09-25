1e6a518249e2289bc652d9b893f3a897f856087d
smoke0030 <ft.priest@gmail.com>
2023-09-25 23:06:07 +0300

    профайл вью сверстан кодом

--- Image Feed/Profile/ProfileViewController.swift
+++ Image Feed/Profile/ProfileViewController.swift
@@ -2,19 +2,67 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    
-    @IBOutlet weak var userAvatar: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var loginLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
+    let imageView = UIImageView(image: UIImage(named: "profile_image"))
+    let nameLabel = UILabel()
+    let loginLabel = UILabel()
+    let descriptionLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addImageView()
+        addNameLabel()
+        addLoginLabel()
+        addDescriptionLabel()
+        addExitButton()
       
     }
     
-    @IBAction func exitButton(_ sender: Any) {
+    func addImageView() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
     }
+    
+    func addNameLabel() {
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        nameLabel.textColor = .white
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.font = UIFont.systemFont(ofSize: 23)
+        nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive =  true
+    }
+    
+    func addLoginLabel() {
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginLabel)
+        loginLabel.textColor = .gray
+        loginLabel.text = "@ekaterina_nov"
+        loginLabel.font = UIFont.systemFont(ofSize: 13)
+        loginLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+    }
+    
+    func addDescriptionLabel() {
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        descriptionLabel.textColor = .white
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13)
+        descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8).isActive = true
+    }
+    
+    func addExitButton() {
+        let exitButton = UIButton()
+        exitButton.setImage(UIImage(systemName: "ipad.and.arrow.forward"), for: .normal)
+        exitButton.tintColor = .red
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(exitButton)
+        exitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        exitButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+    }
+ 
     
 }
