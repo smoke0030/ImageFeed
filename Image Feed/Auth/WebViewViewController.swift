8477ba1dc9e8750a00860ea8c5ad5b2ddf6611df
smoke0030 <ft.priest@gmail.com>
2023-09-28 14:42:57 +0300

    добавлен экран входа

---
+++ Image Feed/Auth/WebViewViewController.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+
+class WebViewViewController {
+    
+let identifier = "ShowWebView"
+    
+}
---
+++ Image Feed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "l3cbbKpfPKOYkdmMI2sl8r2pA4h4zCcjAGDCcxn4sjA"
+let SecretKey = "GCTPWxV4tGgHYfxVPsCfEmgmLfFODL9jH5nxsSD37jY"
+let RedirectURL = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope =  "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://unsplash.com")
